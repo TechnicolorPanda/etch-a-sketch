3a9c432945d3d18a41cb1d5a344df51a66861a7b
Lorri Romesberg <LorriAnn7@att.net>
2020-02-06 15:33:31 -0500

    resized grid from 10 to 100, added random colors

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -5,7 +5,6 @@ function grid(v) {
   const content = document.createElement("div");
   content.classList.add("box");
   container.appendChild(content);
-
   for(var i = 0; i < v; i++){ 
     var row = document.createElement("div"); 
     row.className = "row"; 
@@ -17,11 +16,30 @@ function grid(v) {
       cell.addEventListener("mouseover", changeColor);
       function changeColor(e) {
         const thisCell = e.target;
-        thisCell.style.backgroundColor = "white";
-        console.log(cell);
+        color = randomColor();
+        thisCell.style.backgroundColor = color;
+        thisCell.style.borderColor = color;
       }
       row.appendChild(cell); 
     }
       content.appendChild(row); 
   }
 }
+
+function randomColor() {
+  var x = Math.floor(Math.random() * 256);
+  var y = Math.floor(Math.random() * 256);
+  var z = Math.floor(Math.random() * 256);
+  var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+return bgColor;
+}
+
+function reset() {
+  var cellNumber = prompt ("Select grid size", 16);
+    if (cellNumber >=10 && cellNumber <= 100) {
+      container.innerHTML = "";
+      grid(cellNumber);
+  } else {
+    alert("Select a number between 10 and 100");
+  }
+  }
